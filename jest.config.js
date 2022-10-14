f53d2a7c8d0b8d07a8a46f891d983201728a42ce
Nacho Cebey <n.cebey@gmail.com>
2022-10-14 14:01:23 +0200

    Adding jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+module.exports = {
+	collectCoverage: true,
+	collectCoverageFrom: ['src/**/*.{js,jsx}'],
+	coverageDirectory: 'coverage',
+	testEnvironment: 'jsdom',
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
